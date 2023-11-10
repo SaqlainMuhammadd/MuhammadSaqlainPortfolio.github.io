fb147717ca12d9ee6325eee53cfdb876cf235073
Muhammad Saqlain <miansaqlainqureshi@gmail.com>
2023-11-10 07:28:50 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      Muhammad Saqlain
+      I am here with new settings
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="n-wrapper"></div>
+    )
+}
+export default Navbar;
